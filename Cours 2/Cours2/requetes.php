fb8470a5ffeef4d6c13741b6b0568de9a9adb256
Ober <lucasober@hotmail.fr>
2018-03-14 17:05:08 +0100

    ajout req 5 et correction req 3

--- Cours 2/Cours2/requetes.php
+++ Cours 2/Cours2/requetes.php
@@ -9,9 +9,26 @@
 require_once "vendor/autoload.php";
 \bdd\conf\ConnexionBase::initialisation('src/conf/conf.ini');
 
-//requete 1
 $req1 = \bdd\models\Game::where("id","=","12342")->first();
 $characters = $req1->characters;
 foreach($characters as $char){
 	print($char->name." ".$char->deck."\n");
 }
+
+$req3 = \bdd\models\Company::where("name","like","%Sony%")->get();
+foreach($req3 as $res){
+	$games = $res->games;
+	foreach($games as $game){
+		print($game->name."\n");
+	}
+}
+
+
+$req5 = \bdd\models\Games::where("name","like","Mario%")->get();
+foreach($req5 as $res){
+	$characters = $res->characters;
+	if(sizeOf($characters)>=3){
+		print($res->name."\n");
+	}
+}
+
