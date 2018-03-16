d6e0b14dee16c7f7f8332155339c812fb2bae023
Ober <lucasober@hotmail.fr>
2018-03-16 09:00:24 +0100

    Ajout de la requete 4

--- Cours 3/Cours 3/requetes.php
+++ Cours 3/Cours 3/requetes.php
@@ -9,3 +9,11 @@
 require_once "vendor/autoload.php";
 \bdd\conf\ConnexionBase::initialisation('src/conf/conf.ini');
 
+$timestamp_debut = microtime(true);
+$req6 = \bdd\models\Game::where("name", "like", "Mario%")
+    ->whereHas("game_ratings", function ($q){
+        $q->where("name", "like", "%3+%");
+    })
+    ->get();
+$timestamp_fin = microtime(false);
+$tps_execution = $timestamp_fin-$timestamp_debut;
